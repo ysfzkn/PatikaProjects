e22754df02e53ea7a26a07f729173fb10f9706fc
Yusuf Özkan <iwa-16@sr.dmcs.pl>
2022-05-14 15:11:16 +0200

    v2

--- area_circle.js
+++
@@ -1,9 +0,0 @@
-
-function calculateAreaOfCircle(radius)
-{
-    var area = Math.PI * Math.pow(radius, 2);
-    console.log("Yarıçapı " + radius + " olan dairenin alanı: " + (area).toFixed(3));
-}
-
-const radius = process.argv[2];
-calculateAreaOfCircle(radius);
